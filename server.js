1af521d4bd1c72ce202a6b0ce96447df49ea57f0
Andrei Ce <andreice@Andreis-MacBook-Air-5321.local>
2020-05-13 16:25:25 +0200

    prepared for heroku deployment, now without the app.get('/'...) route before app.get('*'...)

--- server.js
+++ server.js
@@ -10,8 +10,6 @@ connectDB();
 //init Middlewares
 app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) => res.send('API running'));
-
 //define routes
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/posts', require('./routes/api/posts'));
